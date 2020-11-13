547a93e3fb30fe507f9fa3cb2d30129f3fbad7ff
ShrirakshaKS <72904961+ShrirakshaKS@users.noreply.github.com>
2020-11-13 16:32:45 +0530

    Add files via upload

---
+++ Presenter.java
@@ -0,0 +1,8 @@
+public class Presenter
+{
+public static String showcase(String name)
+{
+return name;
+}
+
+}
---
+++ Represent.java
@@ -0,0 +1,8 @@
+public class Represent
+{
+public static void main(String[] show)
+{
+String value=Presenter.showcase("Museum");
+System.out.println(value);
+}
+}
